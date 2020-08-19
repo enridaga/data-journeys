32e524842c19816600022837f90cce87f8c9df3a
enridaga <enricodaga@gmail.com>
2020-08-19 18:54:13 +0100

    Adding test notebook and sample code

---
+++ simple-notebook.py
@@ -0,0 +1,13 @@
+import pandas as pd
+#import time
+#import torchvision
+
+#var1 = time.dosomething()
+#var2 = torchvision.doit()
+var3 = pd.start()
+
+#output = var1 + var2 + var3
+
+output = var3
+
+print(output)
