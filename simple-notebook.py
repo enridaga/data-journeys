defa6cedad9abfb04677585b6ad870b3a4c52f47
enridaga <enricodaga@gmail.com>
2020-08-20 16:27:31 +0100

    New data

--- simple-notebook.py
+++ simple-notebook.py
@@ -5,9 +5,10 @@ import pandas as pd
 #var1 = time.dosomething()
 #var2 = torchvision.doit()
 var3 = pd.start()
-
+var2 = 2
+data = readfile("filename.csv")
 #output = var1 + var2 + var3
 
-output = var3
+output = var3 + var2 + data
 
 print(output)
