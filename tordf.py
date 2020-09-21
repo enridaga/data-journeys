7ec7ce4124f94b264fd2ba24ac1042be55c1a95a
enridaga <enricodaga@gmail.com>
2020-09-21 11:29:09 +0100

    Hashes in RDF uris must be deterministic

--- tordf.py
+++ tordf.py
@@ -27,15 +27,16 @@ f="eda-script-67.py"
 f="practical-model-evaluation-day-1.py" # AttributeError: 'FindDependencies' object has no attribute '_bag'
 f="how-to-attack-a-machine-learning-model.py" # AttributeError: 'Attribute' object has no attribute 'id'
 f="very-simple-pytorch-training-0-59.py" # CLASS support - still to finish
+f="0-cpe-getting-familier-with-problem-and-dataset.py"
 #f="keras-u-net-starter-lb-0-277.py" # TODO AttributeError: 'Call' object has no attribute 'id'
-src = open(indir + f, "r").read()
-# print(src)
-collector = DJ.FindDependencies(f[:-3])
-collector.collect(src)
-s = collector.getStringCollected()
-# Save
-o = open(outdir + f[:-2] + "digraph", "w")
-o.write(s)
+# src = open(indir + f, "r").read()
+# # print(src)
+# collector = DJ.FindDependencies(f[:-3])
+# collector.collect(src)
+# s = collector.getStringCollected()
+# # Save
+# o = open(outdir + f[:-2] + "digraph", "w")
+# o.write(s)
 # # Save
 # #
 g = ag.read_dot(outdir + f[:-2] + "digraph")
